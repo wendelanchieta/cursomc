47af145de691aecf27a05329ce6f72dd6145be52
Wendel Anchieta <wendelanchieta@gmail.com>
2018-10-26 09:49:40 -0200

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/wendelanchieta/cursomc/services/PedidoService.java
@@ -0,0 +1,23 @@
+package com.wendelanchieta.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.wendelanchieta.cursomc.domain.Pedido;
+import com.wendelanchieta.cursomc.repositories.PedidoRepository;
+import com.wendelanchieta.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+
+	public Pedido find(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+
+}
