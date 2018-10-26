0565bbd5c2541b06aeeff9f26952a13a4c298c51
Wendel Anchieta <wendelanchieta@gmail.com>
2018-10-26 12:53:05 -0200

    Atualizando Categoria com PUT. outros pequenos ajustes

--- src/main/java/com/wendelanchieta/cursomc/resources/ClienteResource.java
+++ src/main/java/com/wendelanchieta/cursomc/resources/ClienteResource.java
@@ -19,9 +19,7 @@ public class ClienteResource {
 		
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<Cliente> find(@PathVariable Integer id) {
-
 		Cliente obj = service.find(id);
-
 		return ResponseEntity.ok().body(obj);
 	}
 }
--- src/main/java/com/wendelanchieta/cursomc/services/CategoriaService.java
+++ src/main/java/com/wendelanchieta/cursomc/services/CategoriaService.java
@@ -25,4 +25,8 @@ public class CategoriaService {
 		return repo.save(obj);
 	}
 	
+	public Categoria update(Categoria obj) {
+		find(obj.getId());
+		return repo.save(obj);
+	}
 }
