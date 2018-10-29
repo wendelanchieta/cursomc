63d4f495808664c095dfbf8903b8459b6bc5c0bb
Wendel Anchieta <wendelanchieta@gmail.com>
2018-10-29 10:40:32 -0200

    PUT, DELETE e GET para Cliente

--- src/main/java/com/wendelanchieta/cursomc/dto/CategoriaDTO.java
+++ src/main/java/com/wendelanchieta/cursomc/dto/CategoriaDTO.java
@@ -2,8 +2,9 @@ package com.wendelanchieta.cursomc.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotEmpty;
+
 import org.hibernate.validator.constraints.Length;
-import org.hibernate.validator.constraints.NotEmpty;
 
 import com.wendelanchieta.cursomc.domain.Categoria;
 
--- src/main/java/com/wendelanchieta/cursomc/services/ClienteService.java
+++ src/main/java/com/wendelanchieta/cursomc/services/ClienteService.java
@@ -1,12 +1,20 @@
 package com.wendelanchieta.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
+import org.apache.coyote.http11.filters.IdentityOutputFilter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.wendelanchieta.cursomc.domain.Cliente;
+import com.wendelanchieta.cursomc.dto.ClienteDTO;
 import com.wendelanchieta.cursomc.repositories.ClienteRepository;
+import com.wendelanchieta.cursomc.services.exceptions.DataIntegrityException;
 import com.wendelanchieta.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -18,6 +26,39 @@ public class ClienteService {
 	public Cliente find(Integer id) {
 		Optional<Cliente> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+	
+	public Cliente update(Cliente obj) {
+		Cliente newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir por que há entidades relacionadas");
+		}
+	}
+	
+	public List<Cliente> findAll() {
+		return repo.findAll();
+	}
+	
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy); 
+		return repo.findAll(pageRequest);
+	} 
+	
+	public Cliente fromDTO(ClienteDTO dto) {
+		return new Cliente(dto.getId(), dto.getNome(), dto.getEmail(), null, null);
+	}
+	
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
 	}
 
 }
