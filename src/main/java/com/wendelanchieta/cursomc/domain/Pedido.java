9cba4896e013ba4b58a28b225ab80b4c6d0d2768
Wendel Anchieta <wendelanchieta@gmail.com>
2018-11-02 23:12:56 -0300

    Subtotal e total do pedido

--- src/main/java/com/wendelanchieta/cursomc/domain/Pedido.java
+++ src/main/java/com/wendelanchieta/cursomc/domain/Pedido.java
@@ -54,6 +54,14 @@ public class Pedido implements Serializable {
 		this.cliente = cliente;
 		this.enderecoDeEntrega = enderecoDeEntrega;
 	}
+	
+	public Double getValorTotal() {
+		Double soma = 0.0;
+		for(ItemPedido ip : itens) {
+			soma = soma +ip.getSubTotal();
+		}
+		return soma;
+	}
 
 	public Integer getId() {
 		return id;
