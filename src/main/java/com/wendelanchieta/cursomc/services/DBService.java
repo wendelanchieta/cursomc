1e55b1670b84da2cfc9bccff3ea5732db8cc8b86
Wendel Anchieta <wendelanchieta@gmail.com>
2018-11-09 12:40:59 -0200

    Implementando SmtpEmailService com servidor do Google

--- src/main/java/com/wendelanchieta/cursomc/services/DBService.java
+++ src/main/java/com/wendelanchieta/cursomc/services/DBService.java
@@ -121,7 +121,7 @@ public class DBService {
 		estadoRepository.saveAll(Arrays.asList(est1, est2, est3));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3, c4));
 
-		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
+		Cliente cli1 = new Cliente(null, "Wendel Anchieta", "wendel.anchieta@valec.gov.br", "36378912377", TipoCliente.PESSOAFISICA);
 		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
 
 		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 203", "Jardim", "38220834", cli1, c1);
