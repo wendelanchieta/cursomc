7bc8549df3adb8960d83235be238a4028a1c4289
Wendel Anchieta <wendelanchieta@gmail.com>
2018-11-21 16:13:33 -0200

    Salvando primeiro arquivo no S3

--- src/main/java/com/wendelanchieta/cursomc/CursomcApplication.java
+++ src/main/java/com/wendelanchieta/cursomc/CursomcApplication.java
@@ -1,11 +1,17 @@
 package com.wendelanchieta.cursomc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.wendelanchieta.cursomc.services.S3Service;
+
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
+	
+	@Autowired
+	private S3Service s3Service;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -13,6 +19,6 @@ public class CursomcApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		
+		s3Service.uploadFile("E:\\wendel\\Concursos\\Patricia Quintao\\Aula 00.pdf");
 	}
 }
