0916164500c7446b0b67739229c2118aea9cc1a2
Wendel Anchieta <wendelanchieta@gmail.com>
2018-11-21 13:48:45 -0200

    Esqueci a senha

--- src/main/java/com/wendelanchieta/cursomc/services/EmailService.java
+++ src/main/java/com/wendelanchieta/cursomc/services/EmailService.java
@@ -5,6 +5,7 @@ import javax.mail.internet.MimeMessage;
 
 import org.springframework.mail.SimpleMailMessage;
 
+import com.wendelanchieta.cursomc.domain.Cliente;
 import com.wendelanchieta.cursomc.domain.Pedido;
 
 public interface EmailService {
@@ -16,4 +17,6 @@ public interface EmailService {
 	void sendOrderConfirmationHtmlEmail(Pedido obj);
 	
 	void sendHtmlEmail(MimeMessage msg);
+
+	void sendNewPasswordEmail(Cliente cliente, String newPass);
 }
