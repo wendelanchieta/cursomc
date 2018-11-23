079dfade7fd224445e6853aed901c0d200391d43
Wendel Anchieta <wendelanchieta@gmail.com>
2018-11-23 16:04:59 -0200

    Bonus: ajustando tamanho da imagem

--- src/main/java/com/wendelanchieta/cursomc/services/ClienteService.java
+++ src/main/java/com/wendelanchieta/cursomc/services/ClienteService.java
@@ -36,6 +36,10 @@ public class ClienteService {
 	@Value("${img.prefix.client.profile}")
 	private String prefix;
 	
+	@Value("${img.profile.size}")
+	private Integer size;
+	
+	
 	@Autowired
 	private BCryptPasswordEncoder pe;
 
@@ -126,6 +130,10 @@ public class ClienteService {
 		}
 		
 		BufferedImage jpgImage = imageService.getJpgImageFromFile(multipartFile);
+		jpgImage = imageService.cropSquare(jpgImage);
+		jpgImage = imageService.resize(jpgImage, size);
+		
+		
 		String fileName = prefix + user.getId() + ".jpg";
 		
 		return s3Service.uploadFile(imageService.getInputStream(jpgImage, "jpg"), fileName, "image");
--- src/main/java/com/wendelanchieta/cursomc/services/ImageService.java
+++ src/main/java/com/wendelanchieta/cursomc/services/ImageService.java
@@ -10,6 +10,7 @@ import java.io.InputStream;
 import javax.imageio.ImageIO;
 
 import org.apache.commons.io.FilenameUtils;
+import org.imgscalr.Scalr;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -51,5 +52,27 @@ public class ImageService {
 		} catch (IOException e) {
 			throw new FileException("Erro ao ler arquivo");
 		}
+	}
+	
+	/**
+	 * Recortando a imagem de forma quadrada
+	 * 
+	 * @param sourceImg 
+	 * @return <strong>BufferedImage</strong>
+	 */
+	public BufferedImage cropSquare(BufferedImage sourceImg) {
+		int min = (sourceImg.getHeight() <= sourceImg.getWidth()) ? sourceImg.getHeight() : sourceImg.getWidth();
+		return Scalr.crop(sourceImg, (sourceImg.getWidth()/2) - (min/2), (sourceImg.getHeight()/2) - (min/2), min, min);
+	}
+	
+	/**
+	 * Resize da imagem
+	 * 
+	 * @param sourceImg
+	 * @param size
+	 * @return
+	 */
+	public BufferedImage resize(BufferedImage sourceImg, int size) {
+		return Scalr.resize(sourceImg, Scalr.Method.ULTRA_QUALITY, size);
 	}
 }
