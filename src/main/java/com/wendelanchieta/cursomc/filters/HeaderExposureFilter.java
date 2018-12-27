96675c7e2d84969d137e285ab7828f24b46d4ad9
wendel Anchieta <wendel.anchieta@valec.gov.br>
2018-12-27 17:33:56 -0200

    Expondo o header location nas respostas

--- src/main/java/com/wendelanchieta/cursomc/filters/HeaderExposureFilter.java
+++ src/main/java/com/wendelanchieta/cursomc/filters/HeaderExposureFilter.java
@@ -23,7 +23,7 @@ public class HeaderExposureFilter implements Filter{
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
 		HttpServletResponse res = (HttpServletResponse) response;
-		res.addHeader("access-control-expose-headers", "Authorization");
+		res.addHeader("access-control-expose-headers", "location");
 		chain.doFilter(request, response);
 	}
 
